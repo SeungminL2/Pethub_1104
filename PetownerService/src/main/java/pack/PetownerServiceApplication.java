10befd50a968b2161408baa94a229a63491919f0
SeungminL2 <mslsm111@gmail.com>
2024-11-04 17:05:13 +0900

    Like 관련

---
+++ PetownerService/src/main/java/pack/PetownerServiceApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetownerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetownerServiceApplication.class, args);
+	}
+
+}
